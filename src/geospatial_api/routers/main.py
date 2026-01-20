ffc21ae444239b1df6c4a23fe79d58dfdf0707c3
samhew@ceh.ac.uk <samhew@ceh.ac.uk>
2026-01-20 13:27:24 +0000

    Add colourmap parameter if layer is greyscale, and map centre mapping

--- src/geospatial_api/routers/main.py
+++ src/geospatial_api/routers/main.py
@@ -14,6 +14,17 @@ s3 = get_s3_client()
 
 EXT_MAPPING = {"tif": "raster", "geojson": "vector"}
 
+# Temporary mapping of layer names to map centres to be used until a database is available to provide the information
+DEFAULT_MAP_CENTRE = (54.238, -1.926)  # Roughly the centre of the UK
+LAYER_CENTRES = {
+    "heathstane": (55.520017, -3.392571),
+    "tweedsmuir": (55.515457, -3.414769),
+    "gblcm": (54.238, -1.926),
+    "severn": (52.45808, -3.59893),
+    "chess": (51.71587, -0.58875),
+    "test": (54.008128, -2.774925),
+}
+
 
 @router.get("/available_data")
 def available_data(s3_client: S3Client = Depends(lambda: s3)) -> dict[str, Any]:
@@ -31,6 +42,16 @@ def available_data(s3_client: S3Client = Depends(lambda: s3)) -> dict[str, Any]:
                     "data_type": EXT_MAPPING.get(ext, "unknown"),
                     "s3_url": f"S3://{config.geospatial_data_bucket}/{item['Key']}",
                     "geojson": None,
+                    "map_centre": get_map_centre(name),
+                    "colourmap_name": "terrain" if "greyscale" in name.lower() else None,
                 }
             )
     return JSONResponse(data)
+
+
+def get_map_centre(layer_name: str) -> tuple[float, float]:
+    for name_fragment, layer_centre in LAYER_CENTRES.items():
+        if name_fragment.lower() in layer_name.lower():
+            return layer_centre
+
+    return DEFAULT_MAP_CENTRE
