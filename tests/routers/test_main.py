27e0df4ab3ddb8f0fd742e2f386ed0af96219f09
samhew@ceh.ac.uk <samhew@ceh.ac.uk>
2026-04-07 12:06:26 +0100

    Update test expectations

--- tests/routers/test_main.py
+++ tests/routers/test_main.py
@@ -10,31 +10,82 @@ class TestAvailableData:
         """Test the available_data endpoint returns the expected response."""
         expected_json = [
             {
-                "id": 0,
-                "name": "test_raster_3857_cog_greyscale",
-                "data_type": "raster",
-                "s3_url": "S3://ukceh-fdri-staging-geospatial/raster/test_raster_3857_cog_greyscale.tif",
-                "geojson": None,
-                "map_centre": [54.008128, -2.774925],
-                "colourmap_name": "terrain",
-            },
-            {
                 "id": 1,
-                "name": "test_raster_3857_cog_rendered",
-                "data_type": "raster",
-                "s3_url": "S3://ukceh-fdri-staging-geospatial/raster/test_raster_3857_cog_rendered.tif",
-                "geojson": None,
-                "map_centre": [54.008128, -2.774925],
-                "colourmap_name": None,
+                "name": "Tweed DSM",
+                "project": {"id": 1, "name": "FDRI", "object_key": "fdri"},
+                "date": "2026-03-20",
+                "source_type": {
+                    "id": 1,
+                    "name": "S3",
+                    "object_key": "s3",
+                    "base_url": "s3://ukceh-fdri-staging-geospatial",
+                },
+                "catalogue_id": None,
+                "data_format": {"id": 1, "name": "Raster", "object_key": "raster"},
+                "data_category": {"id": 2, "name": "DSM", "object_key": "dsm"},
+                "legend": [
+                    {"value": 289.97, "colour": [51, 51, 153]},
+                    {"value": 291.61, "colour": [14, 126, 228]},
+                    {"value": 293.25, "colour": [1, 188, 148]},
+                    {"value": 294.9, "colour": [85, 221, 119]},
+                    {"value": 296.54, "colour": [197, 243, 141]},
+                    {"value": 298.18, "colour": [226, 218, 137]},
+                    {"value": 299.83, "colour": [170, 146, 107]},
+                    {"value": 301.47, "colour": [143, 112, 105]},
+                    {"value": 303.11, "colour": [199, 183, 180]},
+                    {"value": 304.76, "colour": [199, 195, 195]},
+                ],
+                "bbox": (
+                    "POLYGON ((-3.417467 55.510475, -3.417269 55.510475, -3.417269 55.510617, -3.417467 55.510617, "
+                    "-3.417467 55.510475))"
+                ),
+                "processing_level": {"id": 1, "name": "Processed", "object_key": "processed"},
+                "area_name": {
+                    "id": 2,
+                    "name": "Tweed",
+                    "object_key": "tweed",
+                    "area_type": {"id": 3, "name": "Catchment", "object_key": "catchment"},
+                },
+                "map_center": [-3.417368, 55.510546],
+                "colour_source_url": (
+                    "s3://ukceh-fdri-staging-geospatial/project=fdri/area_type=catchment/area_name=tweed/"
+                    "data_category=dsm/processing_level=processed/date=2026-03-20/"
+                    "clipped_tweed_dsm_3857_colourised_cog.tif"
+                ),
+                "raw_source_url": None,
             },
             {
                 "id": 2,
-                "name": "test_vector_4326",
-                "data_type": "vector",
-                "s3_url": "S3://ukceh-fdri-staging-geospatial/vector/test_vector_4326.geojson",
-                "geojson": None,
-                "map_centre": [54.008128, -2.774925],
-                "colourmap_name": None,
+                "name": "Cosmos Sites",
+                "project": {"id": 1, "name": "FDRI", "object_key": "fdri"},
+                "date": "2026-03-20",
+                "source_type": {
+                    "id": 1,
+                    "name": "S3",
+                    "object_key": "s3",
+                    "base_url": "s3://ukceh-fdri-staging-geospatial",
+                },
+                "catalogue_id": None,
+                "data_format": {"id": 2, "name": "Vector", "object_key": "vector"},
+                "data_category": {"id": 3, "name": "Stations", "object_key": "stations"},
+                "legend": None,
+                "bbox": (
+                    "POLYGON ((-7.291954 50.03266, 1.034231 50.03266, 1.034231 56.914403, -7.291954 56.914403, "
+                    "-7.291954 50.03266))"
+                ),
+                "processing_level": {"id": 1, "name": "Processed", "object_key": "processed"},
+                "area_name": {
+                    "id": 1,
+                    "name": "UK",
+                    "object_key": "uk",
+                    "area_type": {"id": 1, "name": "National", "object_key": "national"},
+                },
+                "map_center": [-3.1288614999999993, 53.4735315],
+                "colour_source_url": None,
+                "raw_source_url": (
+                    "s3://ukceh-fdri-staging-geospatial/project=fdri/area_type=national/area_name=uk/"
+                    "data_category=stations/processing_level=processed/date=2026-03-20/cosmos_sites.geojson"
+                ),
             },
         ]
 
--- tests/routers/test_vector.py
+++ tests/routers/test_vector.py
@@ -12,7 +12,7 @@ client = TestClient(app)
 
 @pytest.fixture()
 def expected_geojson(data_dir: Path) -> dict[str, Any]:
-    geojson_path = data_dir.joinpath("test_vector_4326.geojson")
+    geojson_path = data_dir.joinpath("cosmos_sites.geojson")
     with open(geojson_path) as geojson_file:
         geojson_data = geojson.load(geojson_file)
 
@@ -22,13 +22,16 @@ def expected_geojson(data_dir: Path) -> dict[str, Any]:
 class TestVector:
     def test_vector_from_s3(self, expected_geojson: dict[str, Any]) -> None:
         """Test the vector endpoint returns valid geojson from s3."""
-        response = client.get("/api/vector?url=S3://ukceh-fdri-staging-geospatial/vector/test_vector_4326.geojson")
+        response = client.get(
+            "/api/vector?url=s3://ukceh-fdri-staging-geospatial/project=fdri/area_type=national/area_name=uk/"
+            "data_category=stations/processing_level=processed/date=2026-03-20/cosmos_sites.geojson"
+        )
         assert response.status_code == 200
         assert response.json() == expected_geojson
 
     def test_vector_from_file_url(self, data_dir: Path, expected_geojson: dict[str, Any]) -> None:
         """Test the vector endpoint returns valid geojson from a file:// url."""
-        geojson_path = data_dir.joinpath("test_vector_4326.geojson")
+        geojson_path = data_dir.joinpath("cosmos_sites.geojson")
         response = client.get(f"/api/vector?url=file:///{geojson_path}")
         assert response.status_code == 200
         assert response.json() == expected_geojson
