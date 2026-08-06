af7188c5b7e844d674fd5622d2bdb4843db2f843
Samantha Hewkin <samhew@ceh.ac.uk>
2026-08-06 16:45:38 +0100

    Add constants file

---
+++ src/geospatial_api/constants.py
@@ -0,0 +1,13 @@
+OPTIONAL_LAYER_FIELDS = [
+    "description",
+    "date",
+    "start_date",
+    "end_date",
+    "colour_source_id",
+    "raw_source_id",
+    "layer_id",
+    "legend",
+    "field_metadata",
+    "filter_metadata",
+    # "resource_metadata",
+]
